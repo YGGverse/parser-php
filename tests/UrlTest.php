f5b1beb51e8557c4f718432d5276519869a2605d
ghost <localhost>
2023-08-07 12:35:57 +0300

    initial commit

---
+++ tests/UrlTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace YGGverse\Parser\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class UrlTest extends TestCase
+{
+  public function testIs(){}
+
+  public function testParse(){}
+}
